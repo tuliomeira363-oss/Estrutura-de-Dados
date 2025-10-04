a0e40f3196f60eee7aebe0e2c7a68d7ebc476c61
tuliomeira363-oss <tuliomeira363@gmail.com>
2025-10-03 21:51:25 -0300

    Delete Exer_04/Exer_04 directory

--- Exer_04/Exer_04/contagem_regressiva.py
+++
@@ -1,9 +0,0 @@
-numero = int(input("Digite um número inteiro positivo: "))
-
-if numero < 0:
-  print ("Número inválido. Por favor, digite um número inteiro POSITIVO.")
-else:
-  print("Contagem regressiva: ")
-  while numero >= 0:
-      print(numero)
-      numero -= 1 
--- Exer_04/Exer_04/tabuada.py
+++
@@ -1,5 +0,0 @@
-nun = int(input("Digite um número: \n\n"))
-i = 1
-while i <= 10:
-  print(nun * i)
-  i += 1
